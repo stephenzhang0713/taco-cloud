e85d6bb6f281850eb9402be52ab46a869a6828bd
Stephen Zhang <stephenzhang0713@outlook.com>
2020-05-17 17:19:40 +0800

    处理订单的提交

--- taco-cloud/src/main/java/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -1,8 +1,10 @@
 package tacos.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Order;
 
@@ -11,13 +13,20 @@ import tacos.Order;
  * @Date: 2020/5/17 16:28
  * @Description:
  */
+@Slf4j
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
 
 	@GetMapping("/current")
 	public String orderForm(Model model) {
-		model.addAttribute("order",new Order());
+		model.addAttribute("order", new Order());
 		return "orderForm";
+	}
+
+	@PostMapping
+	public String processOrder(Order order) {
+		log.info("Order submitted: " + order);
+		return "redirect:/";
 	}
 }
