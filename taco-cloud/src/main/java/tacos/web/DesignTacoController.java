68125b4dcb13077171b45439f7fa648e4ee50a16
Stephen Zhang <stephenzhang0713@outlook.com>
2020-05-17 18:59:04 +0800

    校验POST提交的taco和order

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -3,6 +3,7 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Taco;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -45,23 +47,29 @@ public class DesignTacoController {
 	}
 
 	@GetMapping
-	public String showDesignForm(Model model){
-		model.addAttribute("design",new Taco());
+	public String showDesignForm(Model model) {
+		model.addAttribute("design", new Taco());
 		return "design";
 	}
 
 	@PostMapping
-	public String processDesign(Taco design){
+	public String processDesign(@Valid Taco design, Errors errors) {
+
+		if (errors.hasErrors()){
+			return "design";
+		}
+
 		// save the taco design
 		// we'll do this in chapter 3
-		log.info("Processing design: "+design);
+
+		log.info("Processing design: " + design);
 
 		return "redirect:/orders/current";
 	}
 
 	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
 		return ingredients.stream()
-				.filter(x->x.getType().equals(type))
+				.filter(x -> x.getType().equals(type))
 				.collect(Collectors.toList());
 	}
 
